5575b67ad3870e234615fa05597713cbd05eb0a9
santhosh kumar s <santhosh.your@gmail.com>
2018-03-29 15:31:58 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,25 +1,16 @@
-bgiApp.service('dataService', function () {
-	var studentdata = function (param1, param2) {
-		return  [{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"},{"firstname":"santhosh","lastname":"kumar","emailid":"san@gmail.com","mobile":"9994537084"}];
-		}
-    var employeData = function(){
-		return "santhosh";
-	}
-	return {
-		studentServiceData:studentdata,
-		employeeServiceData: employeData
-	};
-});
-
 bgiApp.controller('elementController', function($scope) {
     $scope.HEADER_TEXT = "Student List";
 });
 
 bgiApp.controller('tableController', ['$scope', 'dataService', function ($scope, dataService) {
     $scope.tableHeader = ['First Name','Last Name','Email Id','Mobile'];
-    $scope.tableData   = dataService.studentServiceData('1','2');
+    $scope.tableData   = dataService.studentdata('1','2');
 }]);
 
 bgiApp.controller('sample', ['$scope', 'dataService', function ($scope, dataService) {
 	$scope.employee = dataService.employeeServiceData();
+}]);
+
+bgiApp.controller('example', ['$scope', 'Inboxservices', function($scope, Inboxservices){
+	$scope.examples = Inboxservices.getData();
 }]);
